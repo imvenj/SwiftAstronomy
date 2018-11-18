637d75f594b7fcea29fa74caf21a9e24923eb9c4
Venj <ersaclarke@gmail.com>
2018-11-18 12:29:12 +0800

    Initial Commit

---
+++ SwiftAstronomy/SwiftAstronomy.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAstronomy.h
+//  SwiftAstronomy
+//
+//  Created by venj on 11/18/18.
+//  Copyright © 2018 venj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftAstronomy.
+FOUNDATION_EXPORT double SwiftAstronomyVersionNumber;
+
+//! Project version string for SwiftAstronomy.
+FOUNDATION_EXPORT const unsigned char SwiftAstronomyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAstronomy/PublicHeader.h>
+
+
